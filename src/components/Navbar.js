4c8cf6ca0469a520f3e27deff09665696d2e9efb
Camila Yokoo <camilayokoo@gmail.com>
2022-09-30 13:19:10 +0200

    basic pages rendering ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,7 @@ function Navbar() {
   return (
     <nav className="Navbar">
       <ul>
-        {/* <Link to="/"> Home </Link> */}
-        {/* <Link to="/about"> About </Link> */}
-        {/* <Link to="/projects"> Projects </Link> */}
         
-        {/*    ADD    */}
         <NavLink to="/" className={({ isActive }) => isActive ? "selected" : ""}>
           Home
         </NavLink>
@@ -18,13 +14,6 @@ function Navbar() {
           className={({ isActive }) => isActive ? "selected" : ""}
          >
           About
-        </NavLink>
-        
-        <NavLink 
-          to="/projects" 
-          className={({ isActive }) => isActive ? "selected" : ""}
-        >
-          Projects
         </NavLink>
         
       </ul>
