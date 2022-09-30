b3ef2fb24a7642f21acabd1b3e2896ae35a5af9a
Camila Yokoo <camilayokoo@gmail.com>
2022-09-30 14:27:24 +0200

    login form printint credentials

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+//import { useNavigate } from "react-router-dom";
+
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+   // const [errorMessage, setErrorMessage] = useState(undefined);
+
+    //const navigate = useNavigate();
+
+    const handleEmail = (e) => setEmail(e.target.value);
+    const handlePassword = (e) => setPassword(e.target.value);
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+        console.log(`email is: ${email}, password is: ${password}`)
+    }
+
+
+
+
+
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="AuthContent">
+        
+        <form className="AuthForm" onSubmit={handleLoginSubmit}>
+            <h1>Login</h1>
+            <label>Email</label>
+            <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+            />
+
+            <label>Password</label>
+            <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePassword}
+            />
+            <button type="submit">Enter</button>
+        </form>
       </div>
     );
   }
