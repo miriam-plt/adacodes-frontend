8564fcd612bc873759845a7e53072f3e41fef452
Camila Yokoo <camilayokoo@gmail.com>
2022-09-30 16:35:50 +0200

    signup integrated with server

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,25 +1,38 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-//import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 function Signup() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-   // const [errorMessage, setErrorMessage] = useState(undefined);
+    const [username, setUsername] = useState("");
+    const [errorMessage, setErrorMessage] = useState(undefined);
 
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
 
     const handleEmail = (e) => setEmail(e.target.value);
     const handlePassword = (e) => setPassword(e.target.value);
+    const handleUsername = (e) => setUsername(e.target.value);
+
+    const API_URL = process.env.REACT_APP_API_URL 
 
     const handleLoginSubmit = (e) => {
         e.preventDefault();
-        console.log(`email is: ${email}, password is: ${password}`)
-    }
-
-
-
+        const requestBody = { email, password, username };
+ 
+    // Make an axios request to the API
+    // If POST request is successful redirect to login page
+    // If the request resolves with an error, set the error message in the state
+    axios.post(`http://localhost:5006/auth/signup`, requestBody)
+      .then((response) => {
+        navigate('/login');
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      })
+    };
 
 
     return (
@@ -29,6 +42,13 @@ function Signup() {
             <h1>Please inform your credentials</h1>
             <p>Already have an account yet?</p>
             <Link to={"/login"}> Log in</Link>
+            <label>Username</label>
+            <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleUsername}
+            />
             <label>Email</label>
             <input
             type="email"
@@ -48,7 +68,7 @@ function Signup() {
             
         </form>
 
-        
+        { errorMessage && <p className="error-message">{errorMessage}</p> }
 
       </div>
     );
