bb4f59dba0d3da2af5347bc43241ff218515ea7f
Camila Yokoo <camilayokoo@gmail.com>
2022-10-04 16:21:15 +0200

    pins rendering successfully

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,32 +1,111 @@
-import React, { useState } from 'react'
+//import { Link } from 'react';
 import  Map, { Marker, Popup } from 'react-map-gl';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const MapComponent = () => {
-    const [showPopup, setShowPopup] = useState(false);
+   const [showPopup, setShowPopup] = useState(false);
 
     const handleDisplayPopup = (e) => { e.originalEvent.stopPropagation(); setShowPopup(!showPopup)}
     const handleClosePopup = () => {setShowPopup(!showPopup)}
-    //[{long: ...., lat:...}, {long: ...., lat:...}, {long: ...., lat:...}]
-  return (
-    <Map
-    initialViewState={{
-      longitude: 13.3888599,
-      latitude: 52.5170365,
-      zoom: 10
-    }}
-    style={{width: '100vw', height: '100vh'}}
-    mapStyle="mapbox://styles/mapbox/streets-v11"
-    mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-  >
-    {!showPopup && <Marker longitude={13.3888599} latitude={ 52.5170365} anchor="bottom" onClick={handleDisplayPopup}>
-    </Marker>}
+
+    const [services, setServices] = useState([]); 
+    const [selectQuery, setSelectQuery] = useState("");
    
-      {showPopup && <Popup longitude={13.3888599} latitude={ 52.5170365} onClose={handleClosePopup}
-        anchor="bottom">
-        You are here
-      </Popup>}
-  </Map>
+    const getAllServices = () => { 
+        axios
+        .get(`${process.env.REACT_APP_API_URL}/api/services`)
+        .then((response) => setServices(response.data))
+        .catch((error) => console.log(error));
+    };
+    
+    useEffect(() => { 
+        getAllServices();
+    }, [] );
+
+    //console.log(services)
+    
+    const handleChange = event => {
+        setSelectQuery(event.target.value)
+    }
+
+    const filteredServices = services.filter(services => {
+        if (selectQuery === "all"){ return services.isApproved === true}
+        if (selectQuery === "events"){ return services.category.includes("events") && services.isApproved === true}
+        if (selectQuery === "groups"){ return services.category.includes("groups") && services.isApproved === true}
+        if (selectQuery === "learning"){ return services.category.includes("learning") && services.isApproved === true}
+        if (selectQuery === "support"){ return services.category.includes("support") && services.isApproved === true}
+        if (selectQuery === "jobs"){ return services.category.includes("jobs") && services.isApproved === true}
+        if (selectQuery === "others"){ return services.category.includes("others") && services.isApproved === true}
+        else {return services.isApproved === true}
+    })
+
+    console.log(filteredServices)
+
+    if(services.length === 0){ 
+    return <p>Loading...</p>
+    }  
+  
+
+  return (
+    <div className="Map">
+        <div>            
+          <button className="allServices-btn" value="all" onClick={handleChange} >All Services</button>
+          <button className="events-btn" value="events" onClick={handleChange} >Events</button>
+          <button className="groups-btn" value="groups" onClick={handleChange} >Groups</button>
+          <button className="learning-btn" value="learning" onClick={handleChange} >Learning</button>
+          <button className="support-btn" value="support" onClick={handleChange}>Support</button>
+          <button className="jobs-btn" value="jobs" onClick={handleChange} >Jobs</button>
+          <button className="others-btn" value="others" onClick={handleChange}>Others</button>
+      </div>
+    
+        <Map
+            initialViewState={{
+            longitude: 13.3888599,
+            latitude: 52.5170365,
+            zoom: 10
+            }}
+            style={{width: '100vw', height: '70vh'}}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        >
+        
+        { filteredServices.map((service) => {
+                return (
+                    <div key={service._id}>
+                        {!showPopup && 
+                        <Marker 
+                            longitude={service.longitude} 
+                            latitude={service.latitude} 
+                            anchor="bottom" 
+                            onClick={handleDisplayPopup}
+                        >
+                    </Marker>}
+                   
+                    {showPopup && 
+                        <Popup 
+                            longitude={service.longitude} 
+                            latitude={service.latitude} 
+                            onClose={handleClosePopup}
+                            anchor="bottom"
+                        >
+                            <img src={service.image} alt="service"></img>
+                            <h3>{service.name}</h3>
+                            <p>{service.street} {service.streetNr}</p>
+                            <p>{service.complement}</p>
+                            <p>{service.email}</p>
+                            <p>{service.website}</p>
+                            <p>{service.phone}</p>
+                            <p>Berlin {service.zip}</p>
+                            {/*<Link>View details</Link>*/}        
+                    </Popup>}
+                    </div>
+                )
+         })
+        }        
+        </Map>
+    </div>
+    
   )
 }
-
 export default MapComponent
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,11 @@
-// import Map from "../components/Map";
 import MapComponent from "../components/MapComponent";
 
+
 function HomePage() {
+  
   return (
     <div>
       <h1>Home</h1>
-      {/* <Map /> */}
       <MapComponent />
     </div>
   );
